064cec99c251dc7d4079f06c09fc3b76a29a34ec
严小格 <694948161@qq.com>
2018-04-23 02:50:51 +0200

    table

--- README.md
+++ README.md
@@ -12,7 +12,17 @@
 ## Start the application 
 
 1. Verify the DB
+
+Before starting this Node services, we should first start our MongoDB Database, to achieve this:
+                
+> docker-compose up
+
+Then all the Data persistent will be saved to ./Mongodb-mount
+
+In a production mode, run docker-compose -d up letting the container run in background. 
+
 2. npm start 
+
 3. Add Datas: python import.py
 
 
--- models/singer.js
+++ models/singer.js
@@ -3,12 +3,16 @@ var Schema = mongoose.Schema;
 
 var singerSchema = new Schema({
     name: { type: String, required: true },
-    singer: { type: String, required: true },
+    alias: { type: String, required: true },
     origin:{ type: String, required: true },
-    yearActive: { type: Date, required: true },
+    genres_str:{ type: String, required: true }
+    labels: { type: String, required: true },
     //addDate: { type: Date, default: Date.now(), required: true },
-    website:{ type: String, required: true },
-    members:{ type: String, required: true },
+    members_str:{ type: String, required: true },
+    link:{ type: String, required: true },
+    img:{ type: String, required: true },
+    size:{ type: Number}
+
 });
 
 var Singer = mongoose.model('Singer', singerSchema);
